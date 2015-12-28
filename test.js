bcaef5e8a3b22b95611d7b87a55a79baa57ecf87
zhouxinkai <846673264@qq.com>
2015-12-28 13:41:22 +0800

    first init HlUI repositoty

---
+++ test.js
@@ -0,0 +1,26 @@
+document.getElementById('id');
+
+/**
+ * @param  {[type]}
+ * @return {[type]}
+ */
+function function_name (argument) {
+	// body...
+}
+$.get('/path/to/file', function(data) {
+	/*optional stuff to do after success */
+});
+var a;
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ * 
+ */
+
+/*
+
+ */
